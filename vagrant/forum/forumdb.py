39b6e1f9ec199ae9a4f1b937ed7ed225e3c1ba82
Lem Dulfo <lemuel.dulfo@gmail.com>
2016-03-26 08:57:07 +0800

    Use database instead of local vars

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,12 @@
 #
 # Database access functions for the web forum.
-# 
-
+#
+import bleach
+import psycopg2
 import time
 
 ## Database connection
-DB = []
+
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +17,14 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
+    cursor.execute("SELECT * FROM posts")
+    results = cursor.fetchall()
+    #print(results)
+    posts = [{'content': (str(row[0])), 'time': str(row[1])} for row in results]
     posts.sort(key=lambda row: row['time'], reverse=True)
+    DB.close();
     return posts
 
 ## Add a post to the database.
@@ -27,5 +34,10 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
     t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    disp = bleach.clean(content)
+    cursor.execute("INSERT INTO posts values (%s)",(disp,))
+    DB.commit()
+    DB.close()
